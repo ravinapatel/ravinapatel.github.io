4a92affa6d927ab4c49b094551382a8791d8f8c2
Ravina Patel <48966108+ravinapatel@users.noreply.github.com>
2023-03-27 22:31:08 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.189699cae5f4a2a25957dc86bc368df9.js"
+  "/precache-manifest.97093c269080aab9c7929d6f51c4db64.js"
 );
 
 self.addEventListener('message', (event) => {
